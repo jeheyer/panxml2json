8a4f1293295e1eb9fae0b5db52ebb7e378d8fb71
J5 <jheyer@opentext.com>
2021-10-20 13:43:42 -0700

    update

--- python/panxml2json.py
+++ python/panxml2json.py
@@ -2,9 +2,9 @@
 device_list_file = "../../../private/cfg/paloaltos.csv"
 
 # Location of panxapi.py 
-#panxapi_location = "./pan-python-0.16.0/bin/panxapi.py"
+panxapi_location = "./pan-python-0.16.0/bin/panxapi.py"
 #panxapi_location = "/mnt/homes/j5/www/panxml2json/python/pan-python-0.16.0/bin/panxapi.py"
-panxapi_location = "/web/www/panxml2json/python/pan-python-0.16.0/bin/panxapi.py"
+#panxapi_location = "/web/www/panxml2json/python/pan-python-0.16.0/bin/panxapi.py"
 
 # Temporary XML file location
 temp_xml_file = "/tmp/xmlapioutput.xml"
