d956c5197dbada173053e28e50099ccc6180e629
J5 <jheyer@opentext.com>
2022-05-21 14:32:24 -0700

    fix relative path handling

--- python/panxml2json.py
+++ python/panxml2json.py
@@ -1,10 +1,10 @@
-import os
+from os import path
 
 # Location of CSR file listing devices
 device_list_file = "../../../private/cfg/paloaltos.csv"
 
 # Location of panxapi.py 
-panxapi_location = "pan-python-0.16.0/bin/panxapi.py"
+panxapi_location = "./pan-python-0.16.0/bin/panxapi.py"
 
 # Temporary XML file location
 temp_xml_file = "/tmp/xmlapioutput.xml"
@@ -13,7 +13,7 @@ def ReadDevices():
 
     devices = []
 
-    PWD = os.path.realpath(path.dirname(__file__))
+    PWD = path.realpath(path.dirname(__file__))
     device_list_file = path.join(PWD, device_list_file)
 
     fh = open(device_list_file, "r")
@@ -39,7 +39,7 @@ def MakeXMLAPICall(hostname, api_key, cli_command):
     for i in range(len(words)-1, -1,-1):
         xml_command += "</"+ words[i] +">"
 
-    PWD = os.path.realpath(path.dirname(__file__))
+    PWD = path.realpath(path.dirname(__file__))
     panxapi_location = path.join(PWD, panxapi_location)
 
     api_command = f"{panxapi_location} -h {hostname} -K \"{api_key}\" -x -o \"{xml_command}\""
